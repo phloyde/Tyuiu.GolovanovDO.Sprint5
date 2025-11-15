f726f7478620d5123f4c8d532a2cfb2ee4582bc3
Tyuiu.GolovanovDO@outlook.com <Tyuiu.GolovanovDO@outlook.com>
2025-11-16 01:25:38 +0500

    t1

---
+++ Tyuiu.GolovanovDO.Sprint5.Task1.V18.Lib/DataService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.GolovanovDO.Sprint5.Task1.V18.Lib
+{
+    public class DataService : ISprint5Task1V18
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+
+            if (fileExist)
+            {
+                File.Delete(path);
+            }
+
+            double y;
+            string strY;
+            
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round(3 * x + 2 - (2 * x - x) / (Math.Cos(x) + 1), 2);
+                double d = (Math.Cos(x) + 1);
+
+                if (d != 0)
+                {
+                    strY = Convert.ToString(y);
+                }
+                else
+                {
+                    strY = Convert.ToString(0);
+                }
+
+
+
+
+                if (x != stopValue)
+                {
+                    File.AppendAllText(path, strY + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, strY);
+                }
+            }
+            
+            return path;    
+        }
+    }
+}
---
+++ Tyuiu.GolovanovDO.Sprint5.Task1.V18/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.GolovanovDO.Sprint5.Task1.V18.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        
+        int starValue = -5;
+        int stopValue = 5;
+
+        Console.WriteLine("Исходные данные: ");
+        Console.WriteLine("starValue = " + starValue + " stopValue = " + stopValue);
+        string res = ds.SaveToFileTextData(starValue, stopValue);
+
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+
+
+    }
+}
