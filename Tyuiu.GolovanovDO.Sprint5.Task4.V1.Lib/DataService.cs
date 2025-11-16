5c9319297e7170ea92020350b5acad681bdf8c2f
Tyuiu.GolovanovDO@outlook.com <Tyuiu.GolovanovDO@outlook.com>
2025-11-16 16:38:41 +0500

    t4

---
+++ Tyuiu.GolovanovDO.Sprint5.Task4.V1.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.GolovanovDO.Sprint5.Task4.V1.Lib
+{
+    public class DataService : ISprint5Task4V1
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            double res = Math.Round(1 - (Math.Cos(Convert.ToInt32(strX)) + Convert.ToInt32(strX)) - 4.12 * Convert.ToInt32(strX), 3);
+            return res;
+
+        }
+    }
+}
---
+++ Tyuiu.GolovanovDO.Sprint5.Task4.V1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.GolovanovDO.Sprint5.Task4.V1.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        string path = @"C:\Users\0\AppData\Local\Temp\InPutDataFileTask4V0.txt";
+
+
+        Console.WriteLine("Данные находятся в файле: ");
+
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
