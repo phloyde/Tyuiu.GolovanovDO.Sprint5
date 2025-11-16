0da0a135d76b27e7e43c043b252ae14300fd625f
Tyuiu.GolovanovDO@outlook.com <Tyuiu.GolovanovDO@outlook.com>
2025-11-17 02:17:20 +0500

    T7

---
+++ Tyuiu.GolovanovDO.Sprint5.Task7.V19.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.GolovanovDO.Sprint5.Task7.V19.Lib
+{
+    public class DataService : ISprint5Task7V19
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string str = File.ReadAllText(path);
+
+            str = str.Replace("сс", "с");
+            str = str.Replace("Сс", "С");
+
+            return str;
+        }
+    }
+}
---
+++ Tyuiu.GolovanovDO.Sprint5.Task7.V19/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.GolovanovDO.Sprint5.Task7.V19.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        string path = @"C:\Users\0\AppData\Local\Temp\InPutDataFileTask7V19.txt";
+
+        Console.WriteLine("Данные находятся в файле: " + path);
+        Console.WriteLine();
+
+        string res = ds.LoadDataAndSave(path);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
