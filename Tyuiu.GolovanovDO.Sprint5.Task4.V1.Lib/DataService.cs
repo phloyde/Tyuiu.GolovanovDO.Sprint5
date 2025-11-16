4a163d886ae2b705f1ac94437953f9cdaeb102c4
Tyuiu.GolovanovDO@outlook.com <Tyuiu.GolovanovDO@outlook.com>
2025-11-16 17:17:29 +0500

    t4

--- Tyuiu.GolovanovDO.Sprint5.Task4.V1.Lib/DataService.cs
+++ Tyuiu.GolovanovDO.Sprint5.Task4.V1.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.GolovanovDO.Sprint5.Task4.V1.Lib
         {
             string strX = File.ReadAllText(path);
             strX = strX.Replace(".", ",");
-            double res = Math.Round(1 - (Math.Cos(double.Parse(strX)) + double.Parse(strX)) - 4.12 * double.Parse(strX), 3);
+            double res = Math.Round(1 / (Math.Cos(double.Parse(strX)) + double.Parse(strX)) - 4.12 * double.Parse(strX), 3);
             return res;
 
         }
